b5fda43b1704c2d752ea8897774c738950459d72
Sayyed Waliullah <jiesm2020@gmail.com>
2022-03-21 22:40:43 +0530

    first commit.............

---
+++ src/demo1/DymmyData.java
@@ -0,0 +1,13 @@
+package demo1;
+
+public class DymmyData {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+for(int i=1;i<=10;i++)
+{
+	System.out.println("this is dymmy Data... to learn Version control System.."+i);
+}
+	}
+
+}
